f8ad39f61476f49986fb4c604f7130b8ef879392
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 15:53:53 -0700

    Operations up to divide

--- calculator.py
+++ calculator.py
@@ -22,15 +22,20 @@ def calculator():
             int_two = int(split_list[2]) #Reassigning index2 as an integer
             if split_list[0] == "+":
                 added_numbers = add(int_one,int_two)
-                return added_numbers
+                print added_numbers
             elif split_list[0] == "-":
                 subt_numbers = subtract(int_one,int_two)
+                print subt_numbers
             elif split_list[0] == "*":
+                mult_numbers = multiply(int_one,int_two)
+                print mult_numbers
             elif split_list[0] == "/":
-            elif split_list[0] == "square":
-            elif split_list[0] == "cube":
-            elif split_list[0] == "power":
-            elif split_list[0] == "mod":
+                div_numbers = divide(int_one,int_two)
+                print div_numbers
+            # elif split_list[0] == "square":
+            # elif split_list[0] == "cube":
+            # elif split_list[0] == "power":
+            # elif split_list[0] == "mod":
 
         
 
