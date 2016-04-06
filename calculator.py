f423be6755ab65d65e50c1ee5b089fdd9bdf24b4
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 15:47:22 -0700

    Add operation

---
+++ calculator.py
@@ -0,0 +1,37 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import * #from the arithmetic file import everything
+
+
+# Your code goes here
+
+def calculator():
+    """Calculating input with different operations"""
+    while True: 
+        input_numbers = raw_input() #Prompting user to add input here
+        split_list = input_numbers.split(" ") #splits input into list
+        if input_numbers == "q": #Allowing user to quit out of program
+            break
+        else:
+            int_one = int(split_list[1]) #Reassigning index1 as an integer
+            int_two = int(split_list[2]) #Reassigning index2 as an integer
+            if split_list[0] == "+":
+                added_numbers = add(int_one,int_two)
+                return added_numbers
+            elif split_list[0] == "-":
+                subt_numbers = subtract(int_one,int_two)
+            elif split_list[0] == "*":
+            elif split_list[0] == "/":
+            elif split_list[0] == "square":
+            elif split_list[0] == "cube":
+            elif split_list[0] == "power":
+            elif split_list[0] == "mod":
+
+        
+
+calculator()
