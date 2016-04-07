eee45e513c0c4e787d7ce9875f239e5cdc043047
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 17:40:32 -0700

    Changed file name

--- calculator.py
+++ calculator.py
@@ -5,21 +5,23 @@ Using our arithmetic.py file from Exercise02, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import * #from the arithmetic file import everything
+from equations import * #from the equations file import everything
 
-
-# Your code goes here
 
 def calculator():
     """Calculating input with different operations"""
     while True: 
         input_numbers = raw_input() #Prompting user to add input here
         split_list = input_numbers.split(" ") #splits input into list
-        for i in range(len(split_list)):
+
+        #Loop through list to change indices 1+ to integer
+        for i in range(len(split_list)): 
             if i > 0:
                 split_list[i] = int(split_list[i])
+
         if input_numbers == "q": #Allowing user to quit out of program
             break
+        #Conditions for different operations
         else: 
             if split_list[0] == "+":
                 added_numbers = add(split_list[1], split_list[2])
